b0e4f96191795553c1a03417b38e5a549d31adb9
sonchez <notreal@notreal.com>
2017-08-28 15:44:31 -0600

    tictactoe_progress

---
+++ lesson5_advanced_ruby_collections/practice_problems_sorting_nested_collections/test.rb
@@ -0,0 +1,18 @@
+
+fulfilled_orders = customer_orders.map do |customer|
+  {
+    customer_id: customer[:customer_id],
+    customer_name: customer[:customer_name]
+  }
+end
+
+customer_orders.each_with_index do |data, index|
+  order_value = data[:orders].reduce(0) do |total, order|
+    total + order[:order_value] if order[:order_fulfilled]
+  end
+
+  fulfilled_orders[index][:order_value] = order_value
+end
+
+
+p fulfilled_orders
